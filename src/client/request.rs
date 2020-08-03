9d7c7c18c0fdbe8da7d080e17c94e791c07ffc91
jacobdenver007 <jacobdenver@163.com>
2020-08-03 11:03:51 +0800

    feature: add muta-sdk in rust

---
+++ src/client/request.rs
@@ -0,0 +1,113 @@
+pub const GET_TRANSACTION: &str = "getTransaction";
+pub const GET_TRANSACTION_QUERY: &str = r#"
+query RpcTransaction($txHash: Hash!) {
+    getTransaction(txHash: $txHash) {
+      chainId
+      cyclesLimit
+      cyclesPrice
+      nonce
+      timeout
+      serviceName
+      method
+      payload
+      txHash
+      pubkey
+      signature 
+    }
+  }
+"#;
+
+pub const GET_BLOCK: &str = "getBlock";
+pub const GET_BLOCK_QUERY: &str = r#"
+query RpcBlock($height: Uint64) {
+    getBlock(height: $height) {
+      header {
+        chainId
+        height
+        execHeight
+        preHash
+        timestamp
+        orderRoot
+        confirmRoot
+        stateRoot
+        receiptRoot
+        cyclesUsed
+        proposer
+        proof {
+          height
+          round
+          blockHash
+          signature
+          bitmap
+        }
+        validatorVersion
+        validators {
+          address
+          proposeWeight
+          voteWeight
+        }
+      }
+      orderedTxHashes
+      hash
+    }
+  }
+"#;
+
+pub const GET_RECEIPT: &str = "getReceipt";
+pub const GET_RECEIPT_QUERY: &str = r#"
+query RpcReceipt($txHash: Hash!) {
+    getReceipt(txHash: $txHash) {
+      stateRoot
+      height    
+      txHash   
+      cyclesUsed
+      events {
+        service
+        topic
+        data
+      }   
+      response {
+        serviceName
+        method
+        response {
+          code
+          succeedData
+          errorMessage
+        }
+      }
+    }
+  }
+"#;
+
+pub const GET_BLOCK_HOOK_RECEIPT: &str = "getBlockHookReceipt";
+pub const GET_BLOCK_HOOK_RECEIPT_QUERY: &str = r#"
+query RpcBlockHookReceipt($height: Uint64!) {
+    getBlockHookReceipt(height: $height) {
+      height
+      stateRoot
+      events {
+        service
+        topic
+        data
+      }   
+    }
+  }
+"#;
+
+pub const SERVICE: &str = "queryService";
+pub const SERVICE_QUERY: &str = r#"
+query RpcService($height: Uint64, $cyclesLimit: Uint64, $cyclesPrice: Uint64, $caller: Address!, $serviceName: String!, $method: String!, $payload: String!) {
+    queryService(height: $height, cyclesLimit: $cyclesLimit, cyclesPrice: $cyclesPrice, caller: $caller, serviceName: $serviceName, method: $method, payload: $payload) {
+      code
+      succeedData
+      errorMessage   
+    }
+  }
+"#;
+
+pub const SEND_TRANSACTION: &str = "sendTransaction";
+pub const SEND_TRANSACTION_MUTATION: &str = r#"
+mutation RpcSendTransaction($input_raw: InputRawTransaction!, $input_encryption: InputTransactionEncryption!) {
+  sendTransaction(inputRaw: $input_raw, inputEncryption: $input_encryption) 
+}
+"#;
